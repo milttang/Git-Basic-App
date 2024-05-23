a100d7004f6287850a79859f15a770e5b166d970
milttang <dntjr043@naver.com>
2024-05-23 16:48:37 +0900

    login 함수 삭제

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 //index.html 을 위한 js file
-function Login(username, password) {
-    if(username)console.log(username);
-    if(password)console.log(password);
-};
-
 function Logout(){
     console.log('>>> logout');
 }
