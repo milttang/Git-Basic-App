0f337785cf96c5e944522732fc1248d01b9407ea
milttang <dntjr043@naver.com>
2024-05-23 16:49:39 +0900

    Revert "login 함수 삭제"
    This reverts commit a100d7004f6287850a79859f15a770e5b166d970.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 //index.html 을 위한 js file
+function Login(username, password) {
+    if(username)console.log(username);
+    if(password)console.log(password);
+};
+
 function Logout(){
     console.log('>>> logout');
 }
